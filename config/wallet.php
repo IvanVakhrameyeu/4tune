f40b9f236c8896949737750a912597f6b8f440f5
Ivan <vano.vach@gmail.com>
2019-08-06 14:28:36 +0300

    settings wallet

---
+++ config/wallet.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'package' => [
+        'rateable' => \Bavix\Wallet\Simple\Rate::class,
+    ],
+    'services' => [
+        'exchange' => \Bavix\Wallet\Services\ExchangeService::class,
+        'common' => \Bavix\Wallet\Services\CommonService::class,
+        'proxy' => \Bavix\Wallet\Services\ProxyService::class,
+        'wallet' => \Bavix\Wallet\Services\WalletService::class,
+    ],
+    'transaction' => [
+        'table' => 'transactions',
+        'model' => \Bavix\Wallet\Models\Transaction::class,
+    ],
+    'transfer' => [
+        'table' => 'transfers',
+        'model' => \Bavix\Wallet\Models\Transfer::class,
+    ],
+    'wallet' => [
+        'table' => 'wallets',
+        'model' => \Bavix\Wallet\Models\Wallet::class,
+        'default' => [
+            'name' => 'Default Wallet',
+            'slug' => 'default',
+        ],
+    ],
+];
