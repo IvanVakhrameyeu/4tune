d34575410688c26f2b2831707fe405ecba16c679
Ivan <vano.vach@gmail.com>
2019-08-11 21:48:21 +0300

    fix

---
+++ app/Jobs/PlayDoubleGame.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class PlayDoubleGame implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $timeout =5;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        dispatch(new PlayDoubleGame())
+            ->onQueue('doubleGameProcessing')
+            ->delay(Carbon::now()->addSeconds(30));
+
+
+    }
+}
